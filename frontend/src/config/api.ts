671ab5d11807c0b7a4cd00283d8c81f1e9eaa616
Ariel Levin <ariel.v.levin@gmail.com>
2024-10-07 12:31:56 +0300

    base url change

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -5,7 +5,8 @@ import axios, {
 } from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL:
+    "http://vercel-scoop-server-arielvlevins-projects.vercel.app:3000/api",
 });
 
 api.interceptors.request.use(
